0113bfbff0ef033bd09aa14b308e2df074740497
Edward Waller <wallerdev@gmail.com>
2009-02-09 15:23:55 -0500

    Initial commit

---
+++ Mechanize.Tests/PageTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Www;
+using MbUnit.Framework;
+
+namespace Www.Tests
+{
+    public class PageTests
+    {
+        [Test]
+        public void TitleShouldBeNullIfNoTitleTag()
+        {
+            Page p = new Page("<html />");
+            Assert.IsNull(p.Title);
+        }
+
+        [Test]
+        public void TitleShouldBeSameAsTitleTag()
+        {
+            Page p = new Page("<html><head><title>Test Title</title></head></html>");
+            Assert.AreEqual("Test Title", p.Title);
+        }
+    }
+}
---
+++ Mechanize/Page.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Www
+{
+    public class Page
+    {
+        public string Html { get; protected set; }
+        public string Title 
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public Page(string html)
+        {
+            Html = html;
+        }
+    }
+}
